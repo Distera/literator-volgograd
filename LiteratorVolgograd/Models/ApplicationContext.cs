e1f725168cec87282eb5fb76c4c3bb79e3c50487
Sergey Burov <sergey.burovic@gmail.com>
2020-04-15 17:35:12 +0400

    Added application database context with PostgreSQL provider and sample entity.

---
+++ LiteratorVolgograd/Models/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LiteratorVolgograd.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=literator;Username=postgres;Password=postgres");
+        }
+    }
+}
