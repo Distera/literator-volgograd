4a4115aea4d07d33a3df3c36fb705afa2dcb7d27
Алена Пьянникова <attapi343@gmail.com>
2020-05-16 11:29:46 +0000

    Implement main section.

---
+++ LiteratorVolgograd/Models/ViewMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace LiteratorVolgograd.Models
+{
+    public class ViewMain
+    {
+        public List<Project> Project { get; set; }
+        public List<News> News { get; set; }
+        public List<Author> Author { get; set; }
+        public List<Publication> Publication { get; set; }
+    }
+}
